07a68742f3dadd1d0ddf5f87ca9f104f8183df9a
Johnnyracket <john.randolph.harrison@gmail.com>
2026-02-06 17:17:19 -0600

    fixed flags.ts bug

--- flags.ts
+++ flags.ts
@@ -1,16 +1,10 @@
 import { flag } from 'flags/next';
 import { createEdgeConfigAdapter } from '@flags-sdk/edge-config';
 
-const adapter = process.env.EDGE_CONFIG
-  ? createEdgeConfigAdapter(process.env.EDGE_CONFIG)<boolean, any>()
-  : undefined;
+const edgeConfigAdapter = createEdgeConfigAdapter(process.env.EDGE_CONFIG!)<boolean, any>()
 
 export const textInputEnabled = flag<boolean>({
   key: 'text-input-enabled',
   description: 'Allow users to paste raw recipe text instead of only URLs',
-  defaultValue: false,
-  ...(adapter ? { adapter } : {}),
-  decide() {
-    return false;
-  },
+  adapter: edgeConfigAdapter
 });
